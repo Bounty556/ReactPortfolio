e8a83329293e0b438d3661f158800f89da3ba34c
Bounty556 <jacobmayday@gmail.com>
2020-09-03 12:42:44 -0600

    Rendered technologies used per project

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import ProjectInfo from './projectInfo';
 
 import './project.css';
+import projectInfo from './projectInfo';
 
 function Project(props) {
+  const [currentProject, setCurrentProject] = useState(ProjectInfo.projects[props.project]);
+
+  useEffect(() => {
+    setCurrentProject(ProjectInfo.projects[props.project]);
+  }, [props.project]);
+
   return (
     <div className='overflow-auto' id='projectContainer'>
-      <div id='technologiesUsed'>NodeJS, MongoDB, Express</div>
-      <div>Div1</div>
-      <div>Div2</div>
-      <div>Div3</div>
+      <div id='technologiesUsed'>
+        {currentProject.technologies.map((tech, i) => (
+          <div className='toolTip' key={i}>
+            <a href={projectInfo.technologies[tech].link} target='_blank' rel='noopener noreferrer'>
+              <img
+                src={'/images/techIcons/' + ProjectInfo.technologies[tech].icon}
+                alt={ProjectInfo.technologies[tech].name}
+              />
+            </a>
+            <span className='toolTipText'>
+              This project uses {ProjectInfo.technologies[tech].name}. Click to find out more.
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
