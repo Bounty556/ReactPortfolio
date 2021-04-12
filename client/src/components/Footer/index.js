0afa8e0db2b7e598b068bc1ee7f9970aadb974ef
Bounty556 <jacobmayday@gmail.com>
2021-04-12 17:01:06 -0600

    Updated copyright

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './footer.css';
 
 function Footer() {
-  return <footer>&#169; 2020 Jacob Peterson</footer>;
+  return <footer>&#169; 2021 Jacob Peterson</footer>;
 }
 
 export default Footer;
